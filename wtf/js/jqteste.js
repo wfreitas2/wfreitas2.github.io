21364cde3aa848858489a15c7507ecbda802abe2
wfreitas2 <you@example.com>
2016-08-08 13:55:07 -0300

    cabeçalho

--- wtf/js/jqteste.js
+++ wtf/js/jqteste.js
@@ -3,13 +3,32 @@ $(window).scroll(function() {
 if ($(this).scrollTop() > 80){  
 
 
-    $("#panel").slideUp(1000);
+    $("#panel").slideUp(400);
 
   }
   
   else{
 
-$("#panel").slideDown(1000);
+$("#panel").slideDown(400);
+
+
+  }
+});
+
+$(window).scroll(function() {
+  
+if ($(this).scrollTop() > 80){  
+
+
+    $("#aparece").show(1000);
+//$("#resto").removeClass("offset1");
+
+  }
+  
+  else{
+
+$("#aparece").hide(1000);
+//    $("#resto").addClass("offset1");
 
 
   }
