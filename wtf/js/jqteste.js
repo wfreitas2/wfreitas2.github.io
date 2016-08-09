34c2c964b198df2f5d306960aa8305d901ac21b8
wfreitas2 <you@example.com>
2016-08-09 14:35:10 -0300

    cabeçalho

--- wtf/js/jqteste.js
+++ wtf/js/jqteste.js
@@ -17,18 +17,18 @@ $("#panel").slideDown(400);
 
 $(window).scroll(function() {
 
-if ($(this).scrollTop() < 80){  
+if ($(this).scrollTop() < 1){  
  
 
 
-    $("#aparece").hide(900);
+    $("#aparece").hide(400);
 //$("#resto").removeClass("offset1");
 
   }
   
   else{
 
-    $("#aparece").show(900);
+    $("#aparece").show(400);
 //    $("#resto").addClass("offset1");
 
 
